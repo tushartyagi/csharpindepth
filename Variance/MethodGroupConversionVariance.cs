aad842390113fa9453a0a0ce63bb55645d6f2eee
tushartyagi <tushar4r@gmail.com>
2015-06-30 19:42:47 +0530

    Method Group Conversion Variance 1

---
+++ Variance/MethodGroupConversionVariance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variance
+{
+    static class MethodGroupConversionVariance
+    {
+        // A delegate which expects the methods to return 
+        // an Animal type.
+        delegate Animal AnimalMaker();
+
+        delegate Animal NameMyAnimal(string professionalName, string funnyName);
+
+        static void Foo()
+        {
+            Giraffe giraffe = new Giraffe();
+
+            // This is an example of method group being covariant in their 
+            // return types. The delegate definition very clearly wants an
+            // Animal from the method group but the definition of MakeGiraffe
+            // returns a giraffe.
+            AnimalMaker animalMaker = new AnimalMaker(giraffe.MakeGiraffe);    
+
+        }
+        
+    }
+}
